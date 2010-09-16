628ead48645efc41bd313c6d225a708c67263192
ferblape <ferblape@gmail.com>
2010-09-16 08:55:46 +0000

    Capistrano files

---
+++ ftadmin/config/deploy.rb
@@ -0,0 +1,16 @@
+set :application, "bioblitz"
+set :repository,  "http://bioblitz.googlecode.com/svn/trunk/"
+set :scm, :subversion
+
+default_run_options[:pty] = true
+
+role :app, "178.79.142.149"
+set :keep_releases, 5
+set :user, 'ubuntu'
+
+set :deploy_to, "/home/ubuntu/www/#{application}"
+
+desc "Restart Application"
+deploy.task :restart, :roles => [:app] do
+  run "touch #{current_path}/tmp/restart.txt"
+end
