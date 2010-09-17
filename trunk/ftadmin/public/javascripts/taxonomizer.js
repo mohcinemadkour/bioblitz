41d7f2cc80204e6535e9ff496339582a45c2ee15
jatorre <jatorre@e73cc812-49db-e9fd-b31e-db4ef6b351a7>
2010-09-17 12:27:09 +0000

    GA

--- trunk/ftadmin/public/javascripts/taxonomizer.js
+++ trunk/ftadmin/public/javascripts/taxonomizer.js
@@ -52,8 +52,9 @@
 
 		function getNextObservationForIdentification() {
 
+        var randomRecord = Math.floor(Math.random()*6)
         //Here we have to check if there is no pending images and tell them THANKS but come back later.
-        sql="select ROWID,observedBy,dateTime,latitude,longitude,occurrenceRemarks,verbatimLocality,zoomitId from 225363 WHERE zoomitId not equal to '' ORDER BY numIdentifications ASC LIMIT 1";
+        sql="select ROWID,observedBy,dateTime,latitude,longitude,occurrenceRemarks,verbatimLocality,zoomitId from 225363 WHERE zoomitId not equal to '' ORDER BY numIdentifications ASC OFFSET "+randomRecord+" LIMIT 1";
           $.ajax({
   	        url: "http://tables.googlelabs.com/api/query?sql="+escape(sql),
   	        dataType: "jsonp",
